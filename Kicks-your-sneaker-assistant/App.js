ed3e0ff5ad3af68f26949b11e27548ef27c03306
Mustafa Raja <mustafa.raja.here@gmail.com>
2023-08-17 06:00:58 +0500

    basic working of three js elements

--- Kicks-your-sneaker-assistant/App.js
+++ Kicks-your-sneaker-assistant/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Canvas } from '@react-three/fiber';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas>
+        <pointLight position={[0, 0, 0]} intensity={1} color={0xffffff} />
+
+      <mesh rotation={[Math.PI / 4, 0, 0]}>
+        <boxGeometry args={[0.7, 0.7]} />
+        <meshBasicMaterial color={0x00ff00} />
+      </mesh>
+    </Canvas>
   );
 }
 
