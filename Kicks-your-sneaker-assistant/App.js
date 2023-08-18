4fdb086fc121c4553b4b8b5949ccb5c976ddc97f
Mustafa Raja <mustafa.raja.here@gmail.com>
2023-08-19 00:02:11 +0500

    pointLight working

--- Kicks-your-sneaker-assistant/App.js
+++ Kicks-your-sneaker-assistant/App.js
@@ -17,8 +17,8 @@ export default function App() {
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
-      {/* <pointLight position={[0, 10, -5]} intensity={1} color="#fff" /> */}
-      <directionalLight intensity={2} position={[10, 10, 20]} color="white" />
+      {/* <pointLight position={[0, 10, -5]} intensity={500} color="#fff" /> */}
+      <directionalLight intensity={1} position={[10, 10, 20]} color="white" />
       <mesh rotation={[Math.PI / 4, 0, 0]}>
         <Box position={[0, 2, 0]} />
         <Box position={[1, 1.2, 0]} />
