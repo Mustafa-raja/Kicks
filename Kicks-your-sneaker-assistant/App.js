e657a02b278a1c8f61f89f29f713456fb28787ee
Mustafa Raja <mustafa.raja.here@gmail.com>
2023-08-18 05:16:20 +0500

    bug fixes, learned a little bit more

--- Kicks-your-sneaker-assistant/App.js
+++ Kicks-your-sneaker-assistant/App.js
@@ -1,24 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
+import { SphereGeometry, MeshStandardMaterial } from 'three';
+
+function Box(props) {
+  return (
+    <mesh {...props}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  );
+}
+
+
+
 export default function App() {
   return (
     <Canvas>
-        <pointLight position={[0, 0, 0]} intensity={1} color={0xffffff} />
-
+      <ambientLight intensity={0.5} />
+      {/* <pointLight position={[0, 10, -5]} intensity={1} color="#fff" /> */}
+      <directionalLight intensity={2} position={[10, 10, 20]} color="white" />
       <mesh rotation={[Math.PI / 4, 0, 0]}>
-        <boxGeometry args={[0.7, 0.7]} />
-        <meshBasicMaterial color={0x00ff00} />
+        <Box position={[2, -1.2, 0]} />
+        <Box position={[0, 1.2, 0]} />
+        {/* <sphereGeometry args={[1, 32, 32]} /> */}
+        <meshStandardMaterial color={'orange'} />
       </mesh>
     </Canvas>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
